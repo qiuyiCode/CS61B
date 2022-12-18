bdbb34c7b0fc7cd3f46c9d4fdf8a46d48595edad
tolearn_linux <1030195712@qq.com>
2022-12-18 19:04:02 +0800

    proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,96 +1,98 @@
 public class ArrayDeque<T> {
-  private T[] items;
-  private int nextFirst;
-  private int nextLast;
-  private int size;
+    private T[] items;
+    private int nextFirst;
+    private int nextLast;
+    private int size;
 
-  public ArrayDeque(){
-    this.items = (T[])new Object[8];
-    this.nextFirst = 0;
-    this.nextLast = 0;
-    this.size = 0;
-  }
-
-  public ArrayDeque(ArrayDeque other){
-    this.items = (T[])new Object[other.items.length];
-    this.nextFirst = other.nextFirst;
-    this.nextLast = other.nextLast;
-    System.arraycopy(other.items,0,this.items,0,other.items.length);
-    this.size = other.size();
-  }
-
-  private void resize(int capacity){
-    T[] tmp = (T[]) new Object[capacity * 2];
-    System.arraycopy(this.items,nextFirst,tmp,nextFirst,this.size);
-    this.items = tmp;
-  }
-
-  public void addFirst(T item){
-    if((this.size() != 0 && this.nextFirst - 1 == this.nextLast)){
-      this.resize(this.items.length * 2);
+    public ArrayDeque() {
+        this.items = (T[]) new Object[8];
+        this.nextFirst = 0;
+        this.nextLast = 0;
+        this.size = 0;
     }
 
-    this.items[nextFirst] = item;
-    this.nextFirst = this.nextFirst - 1;
-
-    if(this.nextFirst == 0){
-      this.nextFirst = this.items.length-1;
+    /*
+    public ArrayDeque(ArrayDeque other) {
+        this.items = (T[]) new Object[other.items.length];
+        this.nextFirst = other.nextFirst;
+        this.nextLast = other.nextLast;
+        System.arraycopy(other.items, 0, this.items, 0, other.items.length);
+        this.size = other.size();
     }
-    this.size++;
-  }
+    */
 
-  public void addLast(T item){
-    if((this.size() != 0 && this.nextFirst + 1 == this.nextLast)){
-      this.resize(this.items.length * 2);
-    }
-    this.items[nextLast] = item;
-    this.nextLast = this.nextLast + 1;
-
-    if(this.nextLast == this.items.length){
-      this.nextLast = (this.nextLast + 1) % this.items.length;
+    private void resize(int capacity) {
+        T[] tmp = (T[]) new Object[capacity * 2];
+        System.arraycopy(this.items, nextFirst, tmp, nextFirst, this.size);
+        this.items = tmp;
     }
 
-    this.size++;
-  }
+    public void addFirst(T item) {
+        if ((this.size() != 0 && this.nextFirst - 1 == this.nextLast)) {
+            this.resize(this.items.length * 2);
+        }
 
-  public boolean isEmpty(){
-    return this.size == 0;
-  }
+        this.items[nextFirst] = item;
+        this.nextFirst = this.nextFirst - 1;
 
-  public int size(){
-    return this.size;
-  }
-
-  public void printDeque(){
-    for (int i = this.nextFirst; i < this.size; i++) {
-      System.out.print(items[i] + " ");
+        if (this.nextFirst == 0) {
+            this.nextFirst = this.items.length - 1;
+        }
+        this.size++;
     }
-  }
 
-  public T removeFirst(){
-    if(this.items[this.nextFirst + 1] == null)
-      return null;
+    public void addLast(T item) {
+        if ((this.size() != 0 && this.nextFirst + 1 == this.nextLast)) {
+            this.resize(this.items.length * 2);
+        }
+        this.items[nextLast] = item;
+        this.nextLast = this.nextLast + 1;
 
-    T t = this.items[this.nextFirst + 1];
-    this.items[this.nextFirst + 1] = null;
-    this.nextFirst = this.nextFirst + 1;
-    this.size--;
-    return t;
-  }
+        if (this.nextLast == this.items.length) {
+            this.nextLast = (this.nextLast + 1) % this.items.length;
+        }
 
-  public T removeLast(){
-    if(this.items[this.nextLast - 1] == null)
-      return null;
+        this.size++;
+    }
 
-    T t = this.items[this.nextLast - 1];
-    this.items[this.nextLast-1] = null;
-    this.size--;
-    return t;
-  }
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
-  public T get(int index){
-    if(index >= this.size)
-      return null;
-    return this.items[index-1];
-  }
+    public int size() {
+        return this.size;
+    }
+
+    public void printDeque() {
+        for (int i = this.nextFirst; i < this.size; i++) {
+            System.out.print(items[i] + " ");
+        }
+    }
+
+    public T removeFirst() {
+        if (this.items[this.nextFirst + 1] == null)
+            return null;
+
+        T t = this.items[this.nextFirst + 1];
+        this.items[this.nextFirst + 1] = null;
+        this.nextFirst = this.nextFirst + 1;
+        this.size--;
+        return t;
+    }
+
+    public T removeLast() {
+        if (this.items[this.nextLast - 1] == null)
+            return null;
+
+        T t = this.items[this.nextLast - 1];
+        this.items[this.nextLast - 1] = null;
+        this.size--;
+        return t;
+    }
+
+    public T get(int index) {
+        if (index >= this.size)
+            return null;
+        return this.items[index - 1];
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -4,152 +4,156 @@
 * 3. size must take constant time.
 */
 public class LinkedListDeque<T> {
-  private class DDLNode {
-    public T item;
-    public DDLNode prev; // previous
-    public DDLNode next;
+    private class DDLNode {
+        public T item;
+        public DDLNode prev; // previous
+        public DDLNode next;
 
-    public DDLNode(T i, DDLNode p, DDLNode n) {
-      this.item = i;
-      this.prev = p;
-      this.next = n;
+        public DDLNode(T i, DDLNode p, DDLNode n) {
+            this.item = i;
+            this.prev = p;
+            this.next = n;
+        }
+
+        public T get(int index) {
+            if (index == 0)
+                return this.item;
+            else if (this.next == sentinel)
+                return null;
+            return this.next.get(--index);
+        }
+    }
+
+    private DDLNode sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        this.sentinel = new DDLNode(null, null, null);
+        this.sentinel.next = sentinel;
+        this.sentinel.prev = this.sentinel;
+        this.size = 0;
+    }
+    /*
+    public LinkedListDeque(LinkedListDeque other) {
+        this.sentinel = new DDLNode(null, null, null);
+        this.sentinel.next = sentinel;
+        this.sentinel.prev = this.sentinel;
+        this.size = 0;
+
+        DDLNode p = other.sentinel.next;
+        while (p != other.sentinel) {
+            this.addLast(p.item);
+            p = p.next;
+        }
+    }
+
+     */
+
+    public void addFirst(T item) {
+        this.sentinel.next = new DDLNode(item, this.sentinel, this.sentinel.next);
+        this.sentinel.next.next.prev = this.sentinel.next;
+        this.size++;
+    }
+
+    public void addLast(T item) {
+        DDLNode node = new DDLNode(item, null, null);
+        node.prev = this.sentinel.prev;
+        node.next = this.sentinel;
+        this.sentinel.prev.next = node;
+        this.sentinel.prev = node;
+        this.size++;
+    }
+
+    public boolean isEmpty() {
+        return this.sentinel.next == this.sentinel;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void printDeque() {
+        if (isEmpty())
+            return;
+
+        DDLNode p = this.sentinel.next;
+        while (p != sentinel) {
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (this.size == 0)
+            return null;
+
+        T t = this.sentinel.next.item;
+        this.sentinel.next.next.prev = this.sentinel;
+        this.sentinel.next = this.sentinel.next.next;
+        this.size--;
+        return t;
+    }
+
+    public T removeLast() {
+        if (isEmpty())
+            return null;
+        T t = this.sentinel.prev.item;
+        this.sentinel.prev.prev.next = this.sentinel.prev.next;
+        this.sentinel.prev = this.sentinel.prev.prev;
+        this.size--;
+        return t;
     }
 
     public T get(int index) {
-      if (index == 0)
-        return this.item;
-      else if (this.next == sentinel)
-        return null;
-      return this.next.get(--index);
-    }
-  }
+        if (this.sentinel.next == this.sentinel)
+            return null;
+        int i = 0;
+        DDLNode p = this.sentinel.next;
+        while (p != null && i != index) {
+            p = p.next;
+            i++;
+        }
 
-  private DDLNode sentinel;
-  private int size;
-
-  public LinkedListDeque() {
-    this.sentinel = new DDLNode(null, null, null);
-    this.sentinel.next = sentinel;
-    this.sentinel.prev = this.sentinel;
-    this.size = 0;
-  }
-
-  public LinkedListDeque(LinkedListDeque<T> other) {
-    this.sentinel = new DDLNode(null, null, null);
-    this.sentinel.next = sentinel;
-    this.sentinel.prev = this.sentinel;
-    this.size = 0;
-
-    DDLNode p = other.sentinel.next;
-    while (p != other.sentinel) {
-      this.addLast(p.item);
-      p = p.next;
-    }
-  }
-
-  public void addFirst(T item) {
-    this.sentinel.next = new DDLNode(item, this.sentinel, this.sentinel.next);
-    this.sentinel.next.next.prev = this.sentinel.next;
-    this.size++;
-  }
-
-  public void addLast(T item) {
-    DDLNode node = new DDLNode(item, null, null);
-    node.prev = this.sentinel.prev;
-    node.next = this.sentinel;
-    this.sentinel.prev.next = node;
-    this.sentinel.prev = node;
-    this.size++;
-  }
-
-  public boolean isEmpty() {
-    return this.sentinel.next == this.sentinel;
-  }
-
-  public int size() {
-    return this.size;
-  }
-
-  public void printDeque() {
-    if (isEmpty())
-      return;
-
-    DDLNode p = this.sentinel.next;
-    while (p != sentinel) {
-      System.out.print(p.item + " ");
-      p = p.next;
-    }
-    System.out.println();
-  }
-
-  public T removeFirst() {
-    T t = this.sentinel.next.item;
-    this.sentinel.next.next.prev = this.sentinel;
-    this.sentinel.next = this.sentinel.next.next;
-    this.size--;
-    return t;
-  }
-
-  public T removeLast() {
-    if (isEmpty())
-      return null;
-    T t = this.sentinel.prev.item;
-    this.sentinel.prev.prev.next = this.sentinel.prev.next;
-    this.sentinel.prev = this.sentinel.prev.prev;
-    this.size--;
-    return t;
-  }
-
-  public T get(int index) {
-    if (this.sentinel.next == this.sentinel)
-      return null;
-    int i = 0;
-    DDLNode p = this.sentinel.next;
-    while (p != null && i != index) {
-      p = p.next;
-      i++;
+        if (p == null)
+            return null;
+        return p.item;
     }
 
-    if (p == null)
-      return null;
-    return p.item;
-  }
-
-  public T getRecursive(int index) {
-    return this.sentinel.next.get(index);
-  }
+    public T getRecursive(int index) {
+        return this.sentinel.next.get(index);
+    }
 }
 /*
-  public static void main(String[] args) {
-    LinkedListDeque<Integer> deque = new LinkedListDeque<>();
-    System.out.println("是否为空:" + deque.isEmpty());
-    deque.addFirst(20);
-    deque.addFirst(15);
-    deque.addFirst(10);
-    deque.addLast(30);
-    System.out.println(deque.removeLast());
-    System.out.println(deque.removeFirst());
-    System.out.print("遍历一次:");
-    deque.printDeque();
-    System.out.println("根据index获取元素:" + deque.getRecusive(0));
-    System.out.println("是否为空:" + deque.isEmpty());
-    System.out.println("size:" + deque.size());
-
-    System.out.println("--------------带参构造函数的测试---------------");
-
-    LinkedListDeque<Integer> CopyDeque = new LinkedListDeque<>(deque);
-    System.out.println("是否为空:" + CopyDeque.isEmpty());
-    CopyDeque.addFirst(3);
-    CopyDeque.addLast(5);
-    CopyDeque.addLast(7);
-    System.out.print("遍历一次:");
-    CopyDeque.printDeque();
-    System.out.println("根据index获取元素index = 0:" + CopyDeque.getRecusive(0));
-    System.out.println("根据index获取元素index = 2:" + CopyDeque.getRecusive(2));
-    System.out.println("是否为空:" + CopyDeque.isEmpty());
-    System.out.println("size:" + CopyDeque.size());
-  }
-}
-
+ * public static void main(String[] args) {
+ * LinkedListDeque<Integer> deque = new LinkedListDeque<>();
+ * System.out.println("是否为空:" + deque.isEmpty());
+ * deque.addFirst(20);
+ * deque.addFirst(15);
+ * deque.addFirst(10);
+ * deque.addLast(30);
+ * System.out.println(deque.removeLast());
+ * System.out.println(deque.removeFirst());
+ * System.out.print("遍历一次:");
+ * deque.printDeque();
+ * System.out.println("根据index获取元素:" + deque.getRecusive(0));
+ * System.out.println("是否为空:" + deque.isEmpty());
+ * System.out.println("size:" + deque.size());
+ * 
+ * System.out.println("--------------带参构造函数的测试---------------");
+ * 
+ * LinkedListDeque<Integer> CopyDeque = new LinkedListDeque<>(deque);
+ * System.out.println("是否为空:" + CopyDeque.isEmpty());
+ * CopyDeque.addFirst(3);
+ * CopyDeque.addLast(5);
+ * CopyDeque.addLast(7);
+ * System.out.print("遍历一次:");
+ * CopyDeque.printDeque();
+ * System.out.println("根据index获取元素index = 0:" + CopyDeque.getRecusive(0));
+ * System.out.println("根据index获取元素index = 2:" + CopyDeque.getRecusive(2));
+ * System.out.println("是否为空:" + CopyDeque.isEmpty());
+ * System.out.println("size:" + CopyDeque.size());
+ * }
+ * }
+ * 
  */
-
