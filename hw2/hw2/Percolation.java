d8d369d5c6f48578ea76cf9013b3dc0d29c4e808
tolearn_linux <1030195712@qq.com>
2023-02-11 13:41:02 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -11,8 +11,10 @@ public class Percolation {
     private int virtualTop = N;
     private int virtualBottom = N + 1;
 
-    public Percolation(int N){
-        if(N <= 0) throw new IllegalArgumentException("N can't smaller than or equals to 0.");
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N can't smaller than or equals to 0.");
+        }
 
         sites = new int[N][N];
         this.N = N;
@@ -25,89 +27,89 @@ public class Percolation {
         }
     }
 
-    private int xyToIndex(int row,int col){
-        if(row < 0 || col < 0 || row >= N || col >= N) {
+    private int xyToIndex(int row, int col) {
+        if (row < 0 || col < 0 || row >= N || col >= N) {
             throw new IndexOutOfBoundsException("row or col must within [0,N-1].");
         }
         return row * N + col;
     }
 
-    private void unionNeighbors(int row,int col,int index){
-        if(row != 0 && this.sites[row-1][col] != blocked){
-            disjointSet.union(xyToIndex(row-1,col),index);
+    private void unionNeighbors(int row, int col, int index) {
+        if (row != 0 && this.sites[row - 1][col] != blocked) {
+            disjointSet.union(xyToIndex(row - 1, col), index);
         }
 
-        if(col != 0 && this.sites[row][col-1] != blocked){
-            disjointSet.union(xyToIndex(row,col-1),index);
+        if (col != 0 && this.sites[row][col - 1] != blocked) {
+            disjointSet.union(xyToIndex(row, col - 1), index);
         }
 
-        if(row != N-1 && this.sites[row+1][col] != blocked){
-            disjointSet.union(index,xyToIndex(row+1,col));
+        if (row != N - 1 && this.sites[row + 1][col] != blocked) {
+            disjointSet.union(index, xyToIndex(row + 1, col));
         }
 
-        if(col != N-1 && this.sites[row][col+1] != blocked){
-            disjointSet.union(index,xyToIndex(row,col+1));
+        if (col != N - 1 && this.sites[row][col + 1] != blocked) {
+            disjointSet.union(index, xyToIndex(row, col + 1));
         }
     }
 
-    public void open(int row,int col){
-        if(row < 0 || col < 0 || row >= N || col >= N) {
+    public void open(int row, int col) {
+        if (row < 0 || col < 0 || row >= N || col >= N) {
             throw new IndexOutOfBoundsException("row or col must within [0,N-1].");
         }
-        if(this.sites[row][col] == blocked) {
-            int index = xyToIndex(row,col);
+        if (this.sites[row][col] == blocked) {
+            int index = xyToIndex(row, col);
             this.sites[row][col] = index;
-            if(row == 0){
-                disjointSet.union(virtualTop,xyToIndex(row,col));
+            if (row == 0) {
+                disjointSet.union(virtualTop, xyToIndex(row, col));
             }
-            if(row == N-1){
-                disjointSet.union(virtualBottom,xyToIndex(row,col));
+            if (row == N - 1) {
+                disjointSet.union(virtualBottom, xyToIndex(row, col));
             }
             this.openSites += 1;
-            unionNeighbors(row,col,index);
+            unionNeighbors(row, col, index);
         }
     }
 
-    public boolean isOpen(int row,int col){
-        if(row < 0 || col < 0 || row >= N || col >= N) {
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || col < 0 || row >= N || col >= N) {
             throw new IndexOutOfBoundsException("row or col must within [0,N-1].");
         }
         return this.sites[row][col] != blocked;
     }
 
-    public boolean isFull(int row,int col){
-        if(row < 0 || col < 0 || row >= N || col >= N) {
+    public boolean isFull(int row, int col) {
+        if (row < 0 || col < 0 || row >= N || col >= N) {
             throw new IndexOutOfBoundsException("row or col must within [0,N-1].");
         }
 
-        if(this.sites[row][col] == blocked){
+        if (this.sites[row][col] == blocked) {
             return false;
         }
 
-        if(disjointSet.connected(virtualTop,xyToIndex(row,col))){
+        if (disjointSet.connected(virtualTop, xyToIndex(row, col))) {
             return true;
         }
         return false;
     }
 
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return this.openSites;
     }
 
-    public boolean percolates(){
-        return this.disjointSet.connected(virtualTop,virtualBottom);
+    public boolean percolates() {
+        return this.disjointSet.connected(virtualTop, virtualBottom);
     }
 
     public static void main(String[] args) {
         Percolation percolation = new Percolation(5);
-        percolation.open(3,4);
-        percolation.open(2,4);
-        percolation.open(2,2);
-        percolation.open(2,3);
-        percolation.open(0,2);
-        percolation.open(1,2);
-        percolation.open(4,4);
-        System.out.println("isFull:" + percolation.isFull(2,2));
+        percolation.open(3, 4);
+        percolation.open(2, 4);
+        percolation.open(2, 2);
+        percolation.open(2, 3);
+        percolation.open(0, 2);
+        percolation.open(1, 2);
+        percolation.open(4, 4);
+        System.out.println("isFull:" + percolation.isFull(2, 2));
         System.out.println("percolates:" + percolation.percolates());
     }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -10,8 +10,11 @@ public class PercolationStats {
     private int T;
     private int N;
     private double[] Xt;
-    public PercolationStats(int N,int T,PercolationFactory pf){
-        if(N <= 0 || T <= 0) throw new IllegalArgumentException("N and T must more than 0.");
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must more than 0.");
+        }
         this.T = T;
         this.N = N;
         Xt = new double[T];
@@ -22,20 +25,20 @@ public class PercolationStats {
         }
     }
 
-    private void experiment_once(int i){
+    private void experiment_once(int i) {
         StdRandom.setSeed(692931);
-        while(percolation.percolates()){
-            int row = StdRandom.uniform(0,N);
-            int col = StdRandom.uniform(0,N);
-            if(!percolation.isOpen(row,col)){
-                percolation.open(row,col);
+        while (percolation.percolates()) {
+            int row = StdRandom.uniform(0, N);
+            int col = StdRandom.uniform(0, N);
+            if (!percolation.isOpen(row, col)) {
+                percolation.open(row, col);
             }
         }
         double xt = percolation.numberOfOpenSites() / (N * N);
         Xt[i] = xt;
     }
 
-    public double mean(){
+    public double mean() {
         double rlt = 0.0;
         for (int i = 0; i < T; i++) {
             rlt += Xt[i];
@@ -44,20 +47,20 @@ public class PercolationStats {
         return rlt;
     }
 
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(Xt);
     }
 
-    private double confidenceHelper(double theta){
+    private double confidenceHelper(double theta) {
         double process = Math.sqrt(T);
         return 1.96 * theta / process;
     }
 
-    public double confidenceLow(){
+    public double confidenceLow() {
         return mean() - confidenceHelper(stddev());
     }
 
-    public double confidenceHigh(){
+    public double confidenceHigh() {
         return mean() + confidenceHelper(stddev());
     }
 }
