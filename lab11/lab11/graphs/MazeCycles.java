77d7d422b5f7bb96f3c01ebc871ebe0891ed0b57
tolearn_linux <1030195712@qq.com>
2023-02-19 19:27:15 +0800

    lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import java.util.Iterator;
+
 /**
  *  @author Josh Hug
  */
@@ -9,14 +11,37 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        this.maze = m;
+    }
+
+    public boolean isCyclicUtil(int v,int parent){
+        marked[v] = true;
+        announce();
+
+        for(int w : maze.adj(v)){
+            if(!marked[w]){
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                if(isCyclicUtil(w,v)){
+                    break;
+                }
+            }else{
+                if(w != parent){
+                    break;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        isCyclicUtil(0,-1);
     }
 
     // Helper methods go here
